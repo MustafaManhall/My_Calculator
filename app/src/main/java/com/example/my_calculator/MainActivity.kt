f4d7bab0c2e42b86062ff8a59304c9f954d618c3
MustafaAldolimy <100897710+DevM0714@users.noreply.github.com>
2023-03-13 21:51:39 +0300

    complete project

---
+++ app/src/main/java/com/example/my_calculator/MainActivity.kt
@@ -0,0 +1,63 @@
+package com.example.my_calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var clearButton: Button
+    lateinit var submitButton: Button
+    lateinit var binaryNumberText: EditText
+    lateinit var hexNumberText: EditText
+    lateinit var decimalNumberText: EditText
+    lateinit var octNumberText: EditText
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        initView()
+        addCallBacks()
+    }
+    private fun addCallBacks() {
+        clearButton.setOnClickListener {
+            clearInput()
+        }
+//        submitButton.setOnClickListener {
+//            onClickSubmitButton(it)
+//        }
+    }
+
+    private fun initView() {
+        clearButton = findViewById(R.id.clearButton)
+        binaryNumberText = findViewById(R.id.binaryNumber)
+        octNumberText = findViewById(R.id.octalNumber)
+        decimalNumberText = findViewById(R.id.decimalNumber)
+        hexNumberText = findViewById(R.id.hexaNumber)
+
+    }
+    private fun clearInput() {
+
+        binaryNumberText.text.clear()
+        octNumberText.text.clear()
+        decimalNumberText.text.clear()
+        hexNumberText.text.clear()
+
+    }
+    fun onClickSubmitButton(v: View) {
+        val decimal = decimalNumberText.text
+        val binary = Integer.toBinaryString(decimal.toString().toInt())
+        binaryNumberText.setText(binary)
+        val hex =Integer.toHexString(decimal.toString().toInt())
+        hexNumberText.setText(hex)
+        val oct = Integer.toOctalString(decimal.toString().toInt())
+        octNumberText.setText(oct)
+    }
+}
